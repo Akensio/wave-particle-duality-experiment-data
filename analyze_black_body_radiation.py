a9b9f9288681e179f49a44f44250166a69770eaf
Gadi Bessudo <gadi.bessudo@coralogix.com>
2025-03-09 22:12:12 +0200

    starting angle analysis

--- analyze_black_body_radiation.py
+++ analyze_black_body_radiation.py
@@ -26,7 +26,7 @@ def main():
     # 1) Load the sheet
     rotation_df = pd.read_excel(datafile, sheet_name="rotation_ratio")
 
-    # 2) Extract the single column (dropping any NaNs, if they exist)
+    # 2) Extract the single column
     col_name = "small_circle_angle_change_per_large_circle_full_rotation-rad"
     rotation_measurements_in_rad = rotation_df[col_name].values
 
@@ -38,9 +38,42 @@ def main():
     angle_ratio = angle_change_per_large_circle_full_rotation_in_rad / (2 * np.pi)  # final ratio (dimensionless)
     angle_ratio_error = angle_change_per_large_circle_full_rotation_error_in_rad / (2 * np.pi)  # uncertainty (SEM) on that ratio
 
-    print(f"Mean small-circle angle per large-circle revolution = {angle_change_per_large_circle_full_rotation_in_rad:.3f} rad")
-    print(f"Std dev = {angle_change_per_large_circle_full_rotation_error_in_rad:.3f} rad")
-    print(f"Dimensionless ratio = {angle_ratio:.5f} ± {angle_ratio_error:.5f}")
+    print("\nRotation Ratio Analysis:")
+    print(f"    Mean small-circle angle per large-circle revolution = {angle_change_per_large_circle_full_rotation_in_rad:.3f} rad")
+    print(f"    Std dev = {angle_change_per_large_circle_full_rotation_error_in_rad:.3f} rad")
+    print(f"    Dimensionless ratio = {angle_ratio:.5f} ± {angle_ratio_error:.5f}")
+
+    ############################################################
+    # STARTING ANGLE
+    ############################################################
+    # 1) Load the sheet
+    starting_angle_df = pd.read_excel(datafile, sheet_name="starting_angle")
+    
+    # 2) Extract the single column
+    col_name = "starting_angle-rad"
+    starting_angle_measurements_in_rad = starting_angle_df[col_name].values
+    
+    # 3) Compute mean + error (with std dev and SEM)
+    starting_angle_in_rad = np.mean(starting_angle_measurements_in_rad)
+    starting_angle_error_in_rad = np.std(starting_angle_measurements_in_rad, ddof=1) / np.sqrt(len(starting_angle_measurements_in_rad))
+    
+    print("\nStarting Angle Analysis:")
+    print(f"    Mean starting angle = {starting_angle_in_rad:.3f} rad")
+    print(f"    Standard error = {starting_angle_error_in_rad:.3f} rad")
+    print(f"    Starting angle = {starting_angle_in_rad:.3f} ± {starting_angle_error_in_rad:.3f} rad")
+
+    ############################################################
+    # RESISTANCE MEASUREMENTS
+    ############################################################
+    # 1) Load the sheet
+    resistance_df = pd.read_excel(datafile, sheet_name="lightbulb_four_wires")
+    
+    # 2) Extract the columns
+    voltage_in_mV = resistance_df["voltage-mV"].values
+    current_in_mA = resistance_df["current-mA"].values
+    
+
+
 
 if __name__ == "__main__":
     main()
